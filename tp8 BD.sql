5370dcab88247e0648c0f444cb1fdb1afacce976
SL-alive <nijimurao811@gmail.com>
2025-11-16 19:51:02 -0300

    Update tp8 BD.sql
    Commit de texto

--- tp8 BD.sql
+++ tp8 BD.sql
@@ -5,6 +5,8 @@ CREATE TABLE tapia_dos(
 	id_tapia INT PRIMARY KEY,
 	siono VARCHAR(100),
 	
+/*Hola profe, este es el commi1t
+Versión inicial del modelo de base de datos*/
 
 );
 
@@ -14,4 +16,5 @@ drop table pepe2;
 create table pepe2(
 	id_pepe INT PRIMARY KEY,
 	siiiii VARCHAR (100)
+
 )
