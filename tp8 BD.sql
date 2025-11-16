104ae562ba8c16f88252ba344947ef878019472c
SL-alive <nijimurao811@gmail.com>
2025-11-16 19:41:20 -0300

    Add files via upload

---
+++ tp8 BD.sql
@@ -0,0 +1,17 @@
+create database pepe
+use pepe;
+
+CREATE TABLE tapia_dos(
+	id_tapia INT PRIMARY KEY,
+	siono VARCHAR(100),
+	
+
+);
+
+
+drop table pepe2;
+
+create table pepe2(
+	id_pepe INT PRIMARY KEY,
+	siiiii VARCHAR (100)
+)
